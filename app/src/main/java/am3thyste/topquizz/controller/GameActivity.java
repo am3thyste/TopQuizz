bcae39bed8e15004705403c1e033cf5fa2762ff5
am3thyste <am3thyste.ursuludmila2010@gmail.com>
2019-11-26 17:37:17 +0100

    working app - final version

--- app/src/main/java/am3thyste/topquizz/controller/GameActivity.java
+++ app/src/main/java/am3thyste/topquizz/controller/GameActivity.java
@@ -1,11 +1,16 @@
 package am3thyste.topquizz.controller;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Arrays;
@@ -20,11 +25,22 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     private Button mButtonAnswer1, mButtonAnswer2, mButtonAnswer3, mButtonAnswer4;
     private QuestionBank mQuestionBank;
     private Question mCurrentQuestion;
+    private int mScore;
+    private int mNumberOfQuestions;
+    public static final String BUNDLE_EXTRA_SCORE="BUNDLE_EXTRA_SCORE";
+
+    private boolean mEnableTouchEvents;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        System.out.println("GameActivity::onCreate()");
+
+        mScore=0;
+        mNumberOfQuestions=4;
+        mEnableTouchEvents=true;
 
         mQuestionTextView = findViewById(R.id.game_activity_question_text);
         mButtonAnswer1 = findViewById(R.id.game_activity_activity1_btn);
@@ -34,9 +50,9 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 
         //use the tag property, to name the buttons:
         mButtonAnswer1.setTag(0);
-        mButtonAnswer1.setTag(1);
-        mButtonAnswer1.setTag(2);
-        mButtonAnswer1.setTag(3);
+        mButtonAnswer2.setTag(1);
+        mButtonAnswer3.setTag(2);
+        mButtonAnswer4.setTag(3);
 
         mButtonAnswer1.setOnClickListener(this);
         mButtonAnswer2.setOnClickListener(this);
@@ -46,6 +62,7 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 
         mQuestionBank=this.generateQuestions();
         mCurrentQuestion=mQuestionBank.getQuestion();
+        this.displayQuestion(mCurrentQuestion);
 
     }
 
@@ -64,35 +81,132 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         //test if the answer is the correct one, or not:
         if(responseIndex == mCurrentQuestion.getAnswerIndex()){
             Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show();
+            mScore++;
         }else{
             Toast.makeText(this, "Wrong answer", Toast.LENGTH_SHORT).show();
         }
+
+        mEnableTouchEvents=false;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mEnableTouchEvents=true;
+
+                //if this is the last question, ends the game
+                //else, display the next question.
+                if(--mNumberOfQuestions==0){
+                    //end of game
+                    endGame();
+                }
+                else{
+                    mCurrentQuestion=mQuestionBank.getQuestion();
+                    displayQuestion(mCurrentQuestion);
+                }
+            }
+        }, 2000);
+
+
     }
 
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        return mEnableTouchEvents && super.dispatchTouchEvent(ev);
+    }
+
+    private void endGame(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Well done!").setMessage("Your score is "+ mScore).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent intent = new Intent();
+                intent.putExtra(BUNDLE_EXTRA_SCORE, mScore);
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        })
+                .create()
+                .show();
+    }
+
+
     private QuestionBank generateQuestions () {
-        Question question1 = new Question("Who is the creator of Android?",
-                Arrays.asList("Andy Rubin",
-                        "Steve Wozniak",
-                        "Jake Wharton",
-                        "Paul Smith"),
+        Question question1 = new Question("What is the name of the current french president?",
+                Arrays.asList("François Hollande", "Emmanuel Macron", "Jacques Chirac", "François Mitterand"),
+                1);
+
+        Question question2 = new Question("How many countries are there in the European Union?",
+                Arrays.asList("15", "24", "28", "32"),
+                2);
+
+        Question question3 = new Question("Who is the creator of the Android operating system?",
+                Arrays.asList("Andy Rubin", "Steve Wozniak", "Jake Wharton", "Paul Smith"),
                 0);
 
-        Question question2 = new Question("When did the first man land on the moon?",
-                Arrays.asList("1958",
-                        "1962",
-                        "1967",
-                        "1969"),
+        Question question4 = new Question("When did the first man land on the moon?",
+                Arrays.asList("1958", "1962", "1967", "1969"),
                 3);
 
-        Question question3 = new Question("What is the house number of The Simpsons?",
-                Arrays.asList("42",
-                        "101",
-                        "666",
-                        "742"),
+        Question question5 = new Question("What is the capital of Romania?",
+                Arrays.asList("Bucarest", "Warsaw", "Budapest", "Berlin"),
+                0);
+
+        Question question6 = new Question("Who did the Mona Lisa paint?",
+                Arrays.asList("Michelangelo", "Leonardo Da Vinci", "Raphael", "Carravagio"),
+                1);
+
+        Question question7 = new Question("In which city is the composer Frédéric Chopin buried?",
+                Arrays.asList("Strasbourg", "Warsaw", "Paris", "Moscow"),
+                2);
+
+        Question question8 = new Question("What is the country top-level domain of Belgium?",
+                Arrays.asList(".bg", ".bm", ".bl", ".be"),
+                3);
+
+        Question question9 = new Question("What is the house number of The Simpsons?",
+                Arrays.asList("42", "101", "666", "742"),
                 3);
 
         return new QuestionBank(Arrays.asList(question1,
                 question2,
-                question3));
+                question3,
+                question4,
+                question5,
+                question6,
+                question7,
+                question8,
+                question9));
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        System.out.println("GameActivity::onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("GameActivity::onResume()");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("GameActivity::onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        System.out.println("GameActivity::onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.out.println("GameActivity::onDestroy()");
+    }
+
+
 }
--- app/src/main/java/am3thyste/topquizz/controller/MainActivity.java
+++ app/src/main/java/am3thyste/topquizz/controller/MainActivity.java
@@ -5,6 +5,7 @@ package am3thyste.topquizz.controller;
  */
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -12,59 +13,134 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import am3thyste.topquizz.R;
 import am3thyste.topquizz.model.User;
 
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
         private TextView mGreetingText;
         private EditText mNameInput;
-        private Button  mPlayButton;
+        private Button mPlayButton;
         private User mUser;
+        public static final int GAME_ACTIVITY_REQUEST_CODE = 0;
+        private SharedPreferences mPreferences;
+        public static final String PREF_KEY_SCORE = "PREF_KEY_SCORE";
+        public static final String PREF_KEY_FIRSTNAME = "PREF_KEY_FIRSTNAME";
 
-       @Override
-protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+                if (GAME_ACTIVITY_REQUEST_CODE == requestCode && RESULT_OK == resultCode) {
+                        //fetch the score from the Intent
+                        int score = data.getIntExtra(GameActivity.BUNDLE_EXTRA_SCORE, 0);
+                        mPreferences.edit().putInt(PREF_KEY_SCORE, score).apply();
 
-        mGreetingText = findViewById(R.id.main_activity_greeting_txt);
-        mNameInput=findViewById(R.id.main_activity_name_input);
-        mPlayButton=findViewById(R.id.main_activity_play_btn);
-
-        mUser = new User();
-
-        mPlayButton.setEnabled(false);
-        mNameInput.addTextChangedListener(new TextWatcher() {
-                @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                        greetUser();
 
                 }
+        }
 
-                @Override
-                public void onTextChanged(CharSequence s, int start, int before, int count) {
-                        mPlayButton.setEnabled(s.toString().length()!=0);
+        private void greetUser() {
+                String firstname = mPreferences.getString(PREF_KEY_FIRSTNAME, null);
+
+                if (null != firstname) {
+                        int score = mPreferences.getInt(PREF_KEY_SCORE, 0);
+
+                        String fulltext = "Welcome back, " + firstname
+                                + "!\nYour last score was " + score
+                                + ", will you do better this time?";
+                        mGreetingText.setText(fulltext);
+                        mNameInput.setText(firstname);
+                        mNameInput.setSelection(firstname.length());
+                        mPlayButton.setEnabled(true);
                 }
+        }
 
-                @Override
-                public void afterTextChanged(Editable s) {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
 
-                }
-        });
+                System.out.println("MainActivity::onCreate()");
 
-        mPlayButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                        String firstname = mNameInput.getText().toString();
-                        mUser.setFirstname(firstname);
-                        //user clicked LET'S PLAY button
-                        Intent gameActivityIntent = new Intent(MainActivity.this, GameActivity.class);
-                        startActivity(gameActivityIntent);
-                }
-        });
+                mGreetingText = findViewById(R.id.main_activity_greeting_txt);
+                mNameInput = findViewById(R.id.main_activity_name_input);
+                mPlayButton = findViewById(R.id.main_activity_play_btn);
+
+                mUser = new User();
+
+                mPreferences = getPreferences(MODE_PRIVATE);
+
+                mPlayButton.setEnabled(false);
+                mNameInput.addTextChangedListener(new TextWatcher() {
+                        @Override
+                        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                        }
+
+                        @Override
+                        public void onTextChanged(CharSequence s, int start, int before, int count) {
+                                mPlayButton.setEnabled(s.toString().length() != 0);
+                        }
+
+                        @Override
+                        public void afterTextChanged(Editable s) {
+
+                        }
+                });
+
+                mPlayButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                                String firstname = mNameInput.getText().toString();
+                                mUser.setFirstname(firstname);
+                                mPreferences.edit().putString(PREF_KEY_FIRSTNAME, mUser.getFirstname()).apply();
+                                //user clicked LET'S PLAY button
+
+                                Intent gameActivityIntent = new Intent(MainActivity.this, GameActivity.class);
+                                startActivityForResult(gameActivityIntent, GAME_ACTIVITY_REQUEST_CODE);
+                        }
+                });
 
 
         }
+
+        @Override
+        protected void onStart() {
+                super.onStart();
+                System.out.println("MainActivity::onStart()");
         }
+
+        @Override
+        protected void onResume() {
+                super.onResume();
+                System.out.println("MainActivity::onResume()");
+
+        }
+
+        @Override
+        protected void onStop() {
+                super.onStop();
+                System.out.println("MainActivity::onStop()");
+
+        }
+
+        @Override
+        protected void onPause() {
+                super.onPause();
+                System.out.println("MainActivity::onPause()");
+
+        }
+
+        @Override
+        protected void onDestroy() {
+                super.onDestroy();
+                System.out.println("MainActivity::onDestroy()");
+
+        }
+}
+
